b3a64ebc71943777e3c9255cd75e21b12c9069e2
Michael Cutshall <mcutshall0@gmail.com>
2021-11-18 11:48:24 -0700

    Updated cronjob execution time

--- cronjob/create_cronjob.sh
+++ cronjob/create_cronjob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo crontab -l > cron_cpy
-sudo echo "* * * * * /usr/bin/python /home/vagrant/app/check-deployment.py" >> cron_cpy
+sudo echo "0 * * * * /usr/bin/python /home/vagrant/app/check-deployment.py" >> cron_cpy
 sudo crontab cron_cpy
 sudo rm -f cron_cpy
